28378b52f29017a8df2ed1456a4c854fc08253a9
Ajala Abdulsamii <kgasta@gmail.com>
2020-07-17 17:01:56 +0100

    feat: edit todo Item

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ const todos = [
   { text: 'Start coding', done: false },
   { text: 'Repeat', done: false }
 ]
+let todoItemIndexInEdit = null
+const currentEdit = {}
 
 /* selectors */
 const todosElement = document.getElementById('todos');
@@ -22,6 +24,11 @@ const todoFormElement = document.getElementById('todo-form');
 const todoInputElement =  document.getElementById('todo-input');
 const todoItemElements = document.getElementsByClassName('todo-item')
 const todoStatElement = document.getElementById('todos-stat')
+const editModalElement = document.getElementById('edit-modal')
+const editFormElement = document.getElementById('edit-form')
+const editStatusElement = document.getElementById('edit-status')
+const editTextElement = document.getElementById('edit-text')
+const cancelEditElement = document.getElementById('cancel-edit')
 
 /* utility functions */
 function renderTodoItems () {
@@ -31,6 +38,7 @@ function renderTodoItems () {
     todosElement.innerHTML += `<div class="todo-item" data-todo_index="${todoIndex}">
       <input type="checkbox" class="todo-status" ${todo.done ? 'checked' : ''} data-todo_index="${todoIndex}">
       <span class="todo-text" data-todo_index="${todoIndex}">${todo.text}</span>
+      <img class="edit-todo" src="media/edit.svg" data-purpose="edit" data-todo_index="${todoIndex}">
       <img class="remove-todo" src="media/delete.svg" data-purpose="delete" data-todo_index="${todoIndex}">
     </div>`
   })
@@ -69,6 +77,19 @@ function removeTodo(index) {
   updateView()
 }
 
+function initEdit(todoIndex) {
+  todoItemIndexInEdit = todoIndex
+
+  editModalElement.classList.add('active')
+  // editStatusElement.checked = todos[todoIndex].done
+  editTextElement.value = todos[todoItemIndexInEdit].text
+}
+
+function editTodoItem(newText) {
+  todos[todoItemIndexInEdit].text = newText
+  updateView()
+}
+
 // event listeners and triggers
 todoFormElement.addEventListener('submit', (event) => {
   event.preventDefault();
@@ -78,22 +99,43 @@ todoFormElement.addEventListener('submit', (event) => {
 });
 
 todosElement.addEventListener('click', (e) => {
-  // console.log('todos clicked!', e)
-  // console.log(e.target.dataset['todo_index'])
-  const clickedTodoIndex = e.target.dataset.todo_index
-
-  if (!clickedTodoIndex) return
-
-  toggleTodoStatus(clickedTodoIndex)
-})
-
-todosElement.addEventListener('click', (e) => {
   const { todo_index, purpose } = e.target.dataset
 
-  if (!todo_index || (!purpose || purpose !== 'delete')) return
+  // ignore click event if no todo item was actually clicked
+  if (!todo_index) return
 
-  removeTodo(todo_index)
+  // toggle todo item status if there's no purpose (edit/delete) detected
+  if (!purpose) {
+    toggleTodoStatus(todo_index)
+  } else {
+    // delete todo item if purpose was to delete or initiate edit todo item if purpose was to edit
+    if (purpose === 'delete') {
+      removeTodo(todo_index)
+    } else if (purpose === 'edit') {
+      initEdit(todo_index)
+    }
+  }
 })
 
+// editStatusElement.addEventListener('input', (e) => {
+//   console.log(e)
+// })
+
+cancelEditElement.addEventListener('click', e => {
+  editModalElement.classList.remove('active')
+})
+
+editFormElement.addEventListener('submit', e => {
+  e.preventDefault()
+
+  const newTodoText = editTextElement.value
+  if (!newTodoText) return
+
+  if (newTodoText !== todos[todoItemIndexInEdit].text) {
+    editTodoItem(newTodoText)
+  }
+
+  editModalElement.classList.remove('active')
+})
 
 setupTodos()
