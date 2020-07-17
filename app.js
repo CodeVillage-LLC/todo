389daf54827d32ec16a1e9440cee1d1919bd38b0
Ajala Abdulsamii <kgasta@gmail.com>
2020-07-17 12:54:35 +0100

    feat: selectors, utils & listeners

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
-console.log('Page is ready!')
-// console.log(document.getElementById('todos').innerHTML)
+// console.log('Page is ready!')
+
+/* selectors */
+const todosElement = document.getElementById('todos');
+const todoFormElement = document.getElementById('todo-form');
+const todoInputElement =  document.getElementById('todo-input');
+const todoItemElements = document.getElementsByClassName('todo-item')
+
+/* utility functions */
+function addTodo (text) {
+  const markup = `<div class="todo-item">
+    <input type="checkbox" class="todo-status">
+    <span class="todo-text">${text}</span>
+  </div>`;
+
+  todosElement.innerHTML += markup;
+}
+
+// event listeners and triggers
+todoFormElement.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const todoText = todoInputElement.value;
+  addTodo(todoText);
+});
+
+console.log(todoItemElements)
